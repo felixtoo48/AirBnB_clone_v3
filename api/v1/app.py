f3fe721a186b40a8957608227684d9d2a17aa51f
felixtoo48 <felixtoo48@gmail.com>
2023-07-31 12:10:33 +0300

    adding import os and changing os.getenv to os.environ,get

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,13 @@
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
+import os
 
 # Create a Flask instance
 app = Flask(__name__)
 
 # Register the blueprint app_views to your Flask instance app
-app.reguster_blueprint(app_views)
+app.register_blueprint(app_views)
 
 # Define a method to handle app teardown
 @app.teardown_appcontext
@@ -18,6 +19,6 @@ def teardown_appcontext(exception):
 
 # Run the Flask server
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
