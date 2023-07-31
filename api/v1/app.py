5bd19d5cd4fcce3fab9482b182401c636b5ccbfe
felixtoo48 <felixtoo48@gmail.com>
2023-07-31 11:37:19 +0300

    adding documentation and changing half colon to full colon on if for syntax corr

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,19 @@ from flask import Flask
 from api.v1.views import app_views
 from models import storage
 
+# Create a Flask instance
 app = Flask(__name__)
+
+# Register the blueprint app_views to your Flask instance app
 app.reguster_blueprint(app_views)
 
+# Define a method to handle app teardown
 @app.teardown_appcontext
 def teardown_appcontext(exception):
 	storage.close()
 
-if __name__ == "__main__";
+# Run the Flask server
+if __name__ == "__main__":
 	host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 	port = int(os.getenv("HBNB_API_PORT", 5000))
 	app.run(host=host, port=port, threaded=True)
