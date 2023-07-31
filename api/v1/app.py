8083b6d277b4dcae2d04c36fa685398993503efb
felixtoo48 <felixtoo48@gmail.com>
2023-07-31 14:26:14 +0300

    updating get environ importation from os

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 from flask import Flask, render_template, jsonify
 from api.v1.views import app_views
 from models import storage
-import os
+from os import getenv
 
 # Create a Flask instance
 app = Flask(__name__)
@@ -27,6 +27,6 @@ def page_not_found(error):
 
 # Run the Flask server
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
