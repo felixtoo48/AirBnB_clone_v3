86f66ac61198a4f746d73457a7df3740a8c70f7c
felixtoo48 <felixtoo48@gmail.com>
2023-07-31 11:52:28 +0300

    updating documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+# Create a route /status on the app_views object
+# that returns a JSON: {"status": "OK"}
 @app_views.route("/status", methods=["GET"])
 def get_status():
-	return jsonify({"status": "OK"})
-
+    return jsonify({"status": "OK"})
